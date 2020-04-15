babca554ecd3177af9b4652b13aa79130abcd609
yonasdangapaelongan <yonasdanga@gmail.com>
2020-04-15 12:18:49 +0700

    ResponsiPemrograman2823

---
+++ Responsi2/Karyawan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Responsi2
+{
+    class Karyawan
+    {
+        public int No { get; set; }
+        public int NIK { get; set; }
+        public string Nama{get; set;}
+        public int GajiBulanan { get; set; }
+
+        public Karyawan (int No,int NIK, string Nama, int GajiBulanan)
+        {
+            this.No = No;
+            this.NIK = NIK;
+            this.Nama = Nama;
+
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+        }
+        public void karyawan()
+        {
+            Console.WriteLine("{0}  {1}  {2} \t {3}",No, NIK, Nama, GajiBulanan);
+        }
+        public void NaikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * GajiBulanan;
+            GajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0}  {1}  {2} \t {3}", No, NIK, Nama, GajiBulanan);
+
+        }
+    }
+}
---
+++ Responsi2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Responsi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama  = Yonas Danga Paelongan");
+            Console.WriteLine("Nim   = 19.11.2823 \n\n\n");
+
+            Karyawan karyawan = new Karyawan(1, 180302123, "Paijo", 3000000);
+            Karyawan karyawan1 = new Karyawan(2, 190302124, "Jono", 2000000);
+
+            Console.WriteLine("No \t NIK/NAMA \t\t Gaji Bulanan");
+            Console.WriteLine("-----------------------------------------------\n");
+            karyawan.karyawan();
+            karyawan1.karyawan();
+
+            Console.WriteLine("\n\n Asyiiiiik Kenaikan Gaji 10%!!\n");
+
+            Console.WriteLine("No \t NIK/Nama \t\t Gaji Bulanan");
+            Console.WriteLine("-----------------------------------------------\n");
+            karyawan.NaikGaji();
+            karyawan1.NaikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
